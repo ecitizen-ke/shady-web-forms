fc4cedc755fdd36c0427201b3dfe765806cf6567
shadykip <shady.kip@outlook.com>
2024-05-24 17:19:29 +0300

    implemented add post functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, flash
 
 app = Flask(__name__)
 
@@ -7,12 +7,27 @@ posts = [
     {"title": "Post 2", "description": "C# is the best"},
 ]
 
+app.config["SECRET_KEY"] = "21e3367163225c129615869b0a84ffd2dc64177564df2062"
+
 
 @app.route("/")
 def index():
     return render_template("index.html", posts=posts)
 
 
-@app.route("/create", methods=["GET", "POST"])
+@app.route("/create")
 def create():
     return render_template("create.html")
+
+
+@app.route("/store", methods=["POST"])
+def store():
+    title = request.form["title"]
+    description = request.form["description"]
+    if not title:
+        flash("Title is required")
+    elif not description:
+        flash("Description is required")
+    else:
+        posts.append({"title": title, "description": description})
+        return redirect(url_for("index"))
